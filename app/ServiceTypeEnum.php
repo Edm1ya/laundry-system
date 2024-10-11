c9f99ed55b8c8f4db99870601ac9a48a722b2c84
Edward Minaya <ed.miya.14@gmail.com>
2024-10-11 18:00:41 -0400

    Customer and Order resource added

---
+++ app/ServiceTypeEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+enum ServiceTypeEnum: string
+{
+    case lavado = 'lavado';
+    case planchado = 'planchado';
+    case secado = 'secado';
+}
---
+++ database/migrations/2024_10_11_213443_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Customer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class)->constrained();
+            $table->integer('garment_quantity');
+            $table->enum("service_type", ["lavado", "planchado", "secado"]);
+            $table->float('unit_price');
+            $table->float('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
